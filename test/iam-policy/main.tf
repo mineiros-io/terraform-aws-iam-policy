f32463fee520bb9fcaf0985b2a40f17106460e66
Marius Tolzmann <m@rius.berlin>
2020-06-02 23:27:15 +0200

    Add unit tests

---
+++ test/iam-policy/main.tf
@@ -0,0 +1,35 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE AN AWS COGNITO USER POOL
+# This example creates an AWS Cognito User Pool that can be used for authentication (identify verification).
+# ---------------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
+# PROVIDER CONFIGURATION
+# ---------------------------------------------------------------------------------------------------------------------
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = var.aws_region
+}
+
+# ------------------------------------------------------------------------------
+# CREATE THE USER POOL
+# ------------------------------------------------------------------------------
+
+module "test" {
+  source = "../../"
+
+  # name of the policy
+  name_prefix = "S3FullAccess"
+
+  # construct the policy document granting access from the following statements
+  policy_statements = [
+    {
+      sid = "S3FullAccess"
+
+      effect    = "Allow"
+      actions   = ["s3:*"]
+      resources = ["*"]
+    }
+  ]
+}
